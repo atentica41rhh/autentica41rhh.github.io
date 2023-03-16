d131ea4296b5766a1c0b9ff0e08ea9df644d172c
atentica41rhh <autenticacion41rhh@gmail.com>
2023-03-16 16:13:05 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCaymPxZ0iMR5MHw776h0UO_Kru2SzqtxE",
+    authDomain: "autentica41hhr1.firebaseapp.com",
+    projectId: "autentica41hhr1",
+    storageBucket: "autentica41hhr1.appspot.com",
+    messagingSenderId: "219254033001",
+    appId: "1:219254033001:web:e9e3738b00d9e2f5bff0f8",
+    measurementId: "G-1KC1RV2WZY"
+    });
